1a55cf291fa99c64142661091b36d3b7336f67aa
Stewart Saunders <stewsaunders@gmail.com>
2026-06-10 07:51:34 +1000

    Scavenger-hunt QA fixes: Deckard clock 3:14, Riddikulus wand, lighthouse chain words, Nostromo PHOS/Lambert, forensic crate + M'Naghten light; SW v4

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // MEQ Palace service worker.
 // Stale-while-revalidate: serve from cache instantly, fetch in background to refresh.
 // Bump CACHE_VERSION to force evict old caches.
-const CACHE_VERSION = 'meq-palace-v3';
+const CACHE_VERSION = 'meq-palace-v4';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
